a3873ce5ff699abf4b05f7f2f7981bc0169ddac1
luis-fe <luis_engp@hotmail.com>
2023-06-28 16:56:41 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import LimpezaFilaPedidos
+import RecarregarBanco
+from flask import Flask, render_template, jsonify, request
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
+
+app = Flask(__name__)
+port = int(os.environ.get('PORT', 5000))
+def my_task():
+    try:
+        # coloque o código que você deseja executar continuamente aqui
+        tamnho, datahora = RecarregarBanco.FilaTags()
+    except:
+        print('falha na automacao - Fila Reposicao')
+
+    print('fim do ciclo')
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=my_task, trigger='interval', seconds=270)
+scheduler.start()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
