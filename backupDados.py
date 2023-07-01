2c533968afc578106102d9a096dcec46306a5f24
luis-fe <luis_engp@hotmail.com>
2023-07-01 14:36:29 -0300

    Inserido dados

---
+++ backupDados.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+def Funcao_Inserir (df_tags, tamanho,tabela, metodo):
+    # Configurações de conexão ao banco de dados
+    database = "Reposicao"
+    user = "postgres"
+    password = "Master100"
+    host = "127.0.0.1"
+    port = "5432"
+
+# Cria conexão ao banco de dados usando SQLAlchemy
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
+
+    # Inserir dados em lotes
+    chunksize = tamanho
+    for i in range(0, len(df_tags), chunksize):
+        df_tags.iloc[i:i + chunksize].to_sql(tabela, engine, if_exists=metodo, index=False , schema='Reposicao')
+
+
+def InserirDados():
+    tagsreposicao = pd.read_csv('tagsreposicao_2023_07_01.csv')
+    print('Iniciando Insercao')
+    tamanho = tagsreposicao['codbarrastag'].size
+    Funcao_Inserir(tagsreposicao,tamanho,'tagsreposicao','append')
+    print('Dados Inseridos com Suecesso!')
+
+InserirDados()
