6149128253d35d5f662f73ed8a0dfb096edfe1eb
luis-fe <luis_engp@hotmail.com>
2023-07-07 15:43:27 -0300

    Atualizacao65

--- main.py
+++ main.py
@@ -141,7 +141,7 @@ scheduler.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    RecarregaPedidos.IncrementarSku()
     dataframe = CalculoNecessidadesEndereco.CarregarSkuAtual()
+    RecarregaPedidos.IncrementarSku()
     CalculoNecessidadesEndereco.AtualizarTabelaPedidosSKU(dataframe)
     app.run(host='0.0.0.0', port=port)
