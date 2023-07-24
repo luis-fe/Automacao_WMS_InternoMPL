9c053f612d0e91ab12edf7792a05bbb97110d536
luis-fe <luis_engp@hotmail.com>
2023-07-24 15:11:16 -0300

    Atualiza Endereco

--- RecarregaPedidos.py
+++ RecarregaPedidos.py
@@ -188,10 +188,10 @@ def LimpezaPedidosSku():
 
 def AtualizarPedidosConferidos():
     conn = ConexaoCSW.Conexao()
-    PedidosSituacao = pd.read_sql("select DISTINCT p.codPedido as codigopedido, codSequencia, 'Em Conferencia' as situacaopedido FROM ped.SugestaoPedItem p "
+    PedidosSituacao = pd.read_sql("select DISTINCT p.codPedido||'-'||p.codSequencia as codigopedido, 'Em Conferencia' as situacaopedido FROM ped.SugestaoPedItem p "
                                   'join ped.SugestaoPed s on s.codEmpresa = p.codEmpresa and s.codPedido = p.codPedido '
                                   'WHERE p.codEmpresa = 1 and p.qtdePecasConf > 0 and s.situacaoSugestao = 2', conn)
-    PedidosSituacao['codigopedido'] =f'{PedidosSituacao["codigopedido"]}/{PedidosSituacao["codSequencia"]}'
+
 
 
     conn2 = ConexaoPostgreMPL.conexao()
