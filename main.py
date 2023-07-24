f61847b10c8925fe94b7cdbe3d9c1dd89fa61067
luis-fe <luis_engp@hotmail.com>
2023-07-24 15:08:08 -0300

    Atualiza Endereco

--- main.py
+++ main.py
@@ -148,5 +148,4 @@ scheduler.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    RecarregaPedidos.SeparacoPedidos()
-    RecarregaPedidos.SugestaoSKU()
+    RecarregaPedidos.IncrementarSku()
