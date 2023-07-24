23ca55e46af28d9a5ce0c8001fb258b6c0fffe7d
luis-fe <luis_engp@hotmail.com>
2023-07-24 14:40:06 -0300

    Atualiza Endereco

--- RecarregaPedidos.py
+++ RecarregaPedidos.py
@@ -19,6 +19,7 @@ def SeparacoPedidos():
     conn = ConexaoCSW.Conexao()
     SugestoesAbertos = pd.read_sql('SELECT codPedido as codPedido2 , dataGeracao,  priorizar, vlrSugestao,situacaosugestao, dataFaturamentoPrevisto  from ped.SugestaoPed  '
                                    'WHERE codEmpresa =1 and situacaoSugestao =2',conn)
+    SugestoesAbertos["codPedido2"] = SugestoesAbertos["codPedido2"].astype(str)
     SugestoesAbertos['codPedido'] =SugestoesAbertos["codPedido2"]+'/'+ SugestoesAbertos.groupby("codPedido2").cumcount() + 1
 
     PedidosSituacao = pd.read_sql("select DISTINCT p.codPedido||'/'||p.codSequencia as codPedido , 'Em Conferencia' as situacaopedido  FROM ped.SugestaoPedItem p "
