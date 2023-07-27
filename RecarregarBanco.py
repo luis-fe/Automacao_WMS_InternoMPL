08c13b12d37c371989109fb5b4cb64d84e67b0d9
luis-fe <luis_engp@hotmail.com>
2023-07-27 08:50:10 -0300

    Atualiza Endereco

--- RecarregarBanco.py
+++ RecarregarBanco.py
@@ -22,7 +22,7 @@ def FilaTags():
         " (SELECT i2.codCor||'-'  FROM cgi.Item2  i2 WHERE i2.Empresa = 1 and  i2.codItem  = t.codReduzido) || "
         "(SELECT i2.descricao  FROM tcp.SortimentosProduto  i2 WHERE i2.codEmpresa = 1 and  i2.codProduto  = t.codEngenharia  and t.codSortimento  = i2.codSortimento) as cor,"
         " (SELECT tam.descricao  FROM cgi.Item2  i2 join tcp.Tamanhos tam on tam.codEmpresa = i2.Empresa and tam.sequencia = i2.codSeqTamanho  WHERE i2.Empresa = 1 and  i2.codItem  = t.codReduzido) as tamanho"
-        " from tcr.TagBarrasProduto t WHERE codEmpresa in (1) and codNaturezaAtual = (5, 7 ,53) and situacao = 3", conn)
+        " from tcr.TagBarrasProduto t WHERE codEmpresa = 1 and codNaturezaAtual = (5, 7 ,53) and situacao = 3", conn)
 
     df_opstotal = pd.read_sql('SELECT top 200000 numeroOP as numeroop , totPecasOPBaixadas as totalop  '
                               'from tco.MovimentacaoOPFase WHERE codEmpresa = 1 and codFase = 236  '
