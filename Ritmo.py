33e8f53f2d34d9874671352d5bc07fc6fcdea28d
luis-fe <luis_engp@hotmail.com>
2023-08-01 13:34:01 -0300

    Atualiza Endereco

--- Ritmo.py
+++ Ritmo.py
@@ -4,8 +4,11 @@ import pandas as pd
 def RelatorioSeparadores(limite):
 
     conn = ConexaoPostgreMPL.conexao()
-    relatorio = pd.read_sql('select  * from "Reposicao".tags_separacao where ritmo is null and dataseparacao is not null and dataseparacao '
-                            " like '2023-08%' order by dataseparacao desc", conn)
+    relatorio = pd.read_sql(
+        'select * from "Reposicao".tags_separacao '
+        'where ritmo is null and dataseparacao is not null and dataseparacao '
+        "like '2023-08%' order by dataseparacao desc", conn
+    )
     relatorio = relatorio.reset_index(drop=True)
 
     relatorio['horario'] = relatorio['dataseparacao'].str.slice(11, 21)
@@ -22,19 +25,17 @@ def RelatorioSeparadores(limite):
     relatorio['horario'] = relatorio['horario'].apply(horario_centecimal)
     relatorio['ritmo'] = relatorio.groupby(['usuario', 'data'])['horario'].diff()
     relatorio['ritmo'] = relatorio['ritmo'] * 3600
-    relatorio.fillna(500, inplace=True)
 
-    for _, row in relatorio.head(limite).iterrows():
-        ritmo = row['ritmo']
-        pedido = row['codpedido']
-        datahora = row['dataseparacao']
-        update = 'UPDATE "Reposicao".tags_separacao ' \
-                 'SET ritmo = %s ' \
-                 'WHERE codpedido = %s AND dataseparacao = %s'
+    # Remova esta linha, pois o ritmo já foi calculado corretamente
+    # relatorio.fillna(500, inplace=True)
 
-        cursor = conn.cursor()
-        cursor.execute(update, (ritmo, pedido, datahora))
-        conn.commit()
-        cursor.close()
+    update = 'UPDATE "Reposicao".tags_separacao ' \
+             'SET ritmo = %s ' \
+             'WHERE codpedido = %s AND dataseparacao = %s'
+
+    cursor = conn.cursor()
+    cursor.executemany(update, relatorio.head(limite)[['ritmo', 'codpedido', 'dataseparacao']].values)
+    conn.commit()
+    cursor.close()
 
     return relatorio
