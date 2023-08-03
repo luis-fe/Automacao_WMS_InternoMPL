7fe35c59b388cc626718eacab837c5eab8cc6be6
luis-fe <luis_engp@hotmail.com>
2023-08-03 08:39:18 -0300

    Atualiza Endereco

--- main.py
+++ main.py
@@ -169,5 +169,5 @@ scheduler.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Ritmo.RelatorioSeparadoresLimite(200)
+    Ritmo.RelatorioSeparadoresLimite(50)
     app.run(host='0.0.0.0', port=port)
