e42dea9071f9e13b7dd7fe36db49814c9aaf876f
luis-fe <luis_engp@hotmail.com>
2023-08-21 09:17:38 -0300

    Atualiza Endereco

--- Ritmo.py
+++ Ritmo.py
@@ -5,7 +5,7 @@ def RelatorioSeparadoresLimite(limite):
 
     conn = ConexaoPostgreMPL.conexao()
     relatorio = pd.read_sql('SELECT datatempo, usuario, codpedido, ritmo as r1 from "Reposicao"."ProducaoSeparadores" where   '
-                            " dataseparacao >= '2023-08-01' "
+                            " dataseparacao >= '2023-08-20' "
                             'order by dataseparacao ', conn)
     if not relatorio.empty:
         relatorio = relatorio.reset_index(drop=True)
