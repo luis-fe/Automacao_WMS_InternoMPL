5fb96702fbb7b21a6c52cbe788dc3e2f4dc8b85a
luis-fe <luis_engp@hotmail.com>
2023-09-13 17:29:12 -0300

    Atualiza Endereco

--- imprimirEtiqueta.py
+++ imprimirEtiqueta.py
@@ -1,15 +1,13 @@
-import os
-import cups #Importe o pacote pycups
+import cups
 
 
 
 def imprimir_pdf(pdf_file):
     conn = cups.Connection()
     printers = conn.getPrinters()
-    printer_name = printers.keys()[0]
+    printer_name = list(printers.keys())[0]
     job_id = conn.printFile(printer_name,pdf_file,"Etiqueta",{'PageSize': 'Custom.10x2.5cm', 'FitToPage': True, 'Scaling': 100})
     print(f"ID {job_id} enviado para impressão")
 
 
 imprimir_pdf("305815-1.pdf")
-os.remove("305815-1.pdf")
