cfe1fb564cd2fb9c38da6cb0d9fc3ef8e4ecf120
luis-fe <luis_engp@hotmail.com>
2023-09-13 17:43:57 -0300

    Atualiza Endereco

--- imprimirEtiqueta.py
+++ imprimirEtiqueta.py
@@ -6,7 +6,7 @@ def imprimir_pdf(pdf_file):
     conn = cups.Connection()
     printers = conn.getPrinters()
     printer_name = list(printers.keys())[0]
-    job_id = conn.printFile(printer_name,pdf_file,"Etiqueta",{'PageSize': 'Custom.10x2.5cm', 'FitToPage': True, 'Scaling': 100})
+    job_id = conn.printFile(printer_name,pdf_file,"Etiqueta",{'PageSize': 'Custom.10x2.5cm', 'FitToPage': 'True', 'Scaling': '100'})
     print(f"ID {job_id} enviado para impressão")
 
 
