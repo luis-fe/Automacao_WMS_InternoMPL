222bd5b2f3c6357af9ae94a2b0bef9a7e3e43328
luis-fe <luis_engp@hotmail.com>
2023-09-14 10:37:07 -0300

    Atualiza Endereco

--- imprimirEtiqueta.py
+++ imprimirEtiqueta.py
@@ -10,8 +10,8 @@ import qrcode
 
 def criar_pdf(saida_pdf, titulo, cliente, pedido, transportadora):
     # Configurações das etiquetas e colunas
-    label_width = 10 * cm
-    label_height = 2.5 * cm
+    label_width = 8 * cm
+    label_height = 2.0 * cm
 
     # Configurações do QR code
     qr_code_width = label_width
