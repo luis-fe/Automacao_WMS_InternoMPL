d8982e6d83e71d56d54b2268b272d93d32e68c95
luis-fe <luis_engp@hotmail.com>
2023-09-14 11:28:45 -0300

    Atualiza Endereco

--- imprimirEtiqueta.py
+++ imprimirEtiqueta.py
@@ -41,11 +41,11 @@ def criar_pdf(saida_pdf, titulo, cliente, pedido, transportadora):
         barcode_code128.drawOn(c, 2.2 * cm, 0.3 * cm)
 
         c.setFont("Helvetica", 9)
-        c.drawString(0.3 * cm, 1.5 * cm, "Nº Cliente:")
+        c.drawString(0.3 * cm, 1.4 * cm, "Nº Cliente:")
         c.drawString(0.3 * cm, 1 * cm, "Nº Pedido:")
         c.drawString(0.3 * cm, 0.3 * cm, transportadora)
 
-        c.drawString(2.0 * cm, 1.5 * cm, cliente)
+        c.drawString(2.0 * cm, 1.4 * cm, cliente)
         c.drawString(2.0 * cm, 1 * cm, pedido)
 
         c.save()
