792e342181c305e0a6a6ac2a68fac13e65982672
luis-fe <luisengp@gmail.com>
2024-01-10 11:11:18 -0300

    Atualiza Endereco

--- TagsDisponivelGarantia.py
+++ TagsDisponivelGarantia.py
@@ -9,7 +9,7 @@ def BuscarTagsGarantia():
     consulta = pd.read_sql('SELECT '
                            'p.codBarrasTag as codbarrastag , p.codReduzido as codreduzido, p.codEngenharia as engenharia,'
                            ' (select i.nome from cgi.Item i WHERE i.codigo = p.codReduzido) as descricao, situacao, codNaturezaAtual as natureza, codEmpresa as codempresa,'
-                           ' (select s.corbase||'-'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento)'
+                           ' (select s.corbase||'+'-'+'||s.nomecorbase  from tcp.SortimentosProduto s WHERE s.codempresa = 1 and s.codproduto = p.codEngenharia and s.codsortimento = p.codSortimento)'
                            ' as cor, (select t.descricao from tcp.Tamanhos t WHERE t.codempresa = 1 and t.sequencia = p.seqTamanho ) as tamanho, p.numeroOP as numeroop'
                            ' from Tcr.TagBarrasProduto p'
                            'WHERE p.codEmpresa = 1 and '
