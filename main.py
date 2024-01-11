048412b9548dd1dd1c6bb6c2f7387e800d05ebda
luis-fe <luisengp@gmail.com>
2024-01-11 08:09:44 -0300

    Atualiza Endereco

--- main.py
+++ main.py
@@ -170,6 +170,11 @@ scheduler.start()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    TagsDisponivelGarantia.SalvarTagsNoBancoPostgre()
+    try:
+        TagsDisponivelGarantia.SalvarTagsNoBancoPostgre()
+    except Exception as e:
+        print(f"Erro detectado: {str(e)}")
+        restart_server()
+
     my_task()
     app.run(host='0.0.0.0', port=port)
