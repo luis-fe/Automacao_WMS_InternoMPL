2aa479163f3a0c2d83a55281ddb5f2fb728fae4a
luis-fe <luisengp@gmail.com>
2024-03-15 09:45:51 -0300

    commit incrementado dados opsc

--- main.py
+++ main.py
@@ -273,6 +273,7 @@ if __name__ == '__main__':
         limite = 60 * 60  # (limite de 60 minutos , convertido para segundos)
 
         print('ETAPA 2: SUBSTITUTOS DAS OPS')
+        print(tempo)
         if tempo > limite:
             print('iniciando o salvamento')
             MateriaisSubstitutosPorSku.SubstitutosSkuOP()
