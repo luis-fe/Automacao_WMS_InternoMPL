d26cac773a4fdced51383810c076e860f6457a9c
luis-fe <luisengp@gmail.com>
2024-03-19 08:29:30 -0300

    commit incrementado dados opsc

--- main.py
+++ main.py
@@ -189,9 +189,9 @@ def my_task2():
     limite = 90 * 60  # (limite de 90 minutos , convertido para segundos)
     situacaoAutomacao = controle.distinctStatus('ComponentesPrincipalPorSKU')
     if tempo > limite and situacaoAutomacao == 'nao iniciado':
-            controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
-            MateriaisSubstitutosPorSku.ComponentesPrincipalPorSKU()
-            controle.salvarStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
+           # controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
+           # MateriaisSubstitutosPorSku.ComponentesPrincipalPorSKU()
+           # controle.salvarStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
 
     else:
 
@@ -319,9 +319,9 @@ if __name__ == '__main__':
         limite = 90 * 60  # (limite de 90 minutos , convertido para segundos)
         situacaoAutomacao = controle.distinctStatus('ComponentesPrincipalPorSKU')
         if tempo > limite and situacaoAutomacao == 'nao iniciado':
-            controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
-            MateriaisSubstitutosPorSku.ComponentesPrincipalPorSKU()
-            controle.salvarStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
+            #controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
+            #MateriaisSubstitutosPorSku.ComponentesPrincipalPorSKU()
+            #controle.salvarStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
 
 
 
