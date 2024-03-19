d7d6f510f17c49026a2d2fb01c7d56b9d0d77cf5
luis-fe <luisengp@gmail.com>
2024-03-19 08:30:37 -0300

    commit incrementado dados opsc

--- main.py
+++ main.py
@@ -189,10 +189,10 @@ def my_task2():
     limite = 90 * 60  # (limite de 90 minutos , convertido para segundos)
     situacaoAutomacao = controle.distinctStatus('ComponentesPrincipalPorSKU')
     if tempo > limite and situacaoAutomacao == 'nao iniciado':
-        # controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
+            # controle.InserindoStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
            # MateriaisSubstitutosPorSku.ComponentesPrincipalPorSKU()
            # controle.salvarStatus('ComponentesPrincipalPorSKU', client_ip, datainicio)
-
+        print('tesgt')
     else:
 
             print('JA EXISTE UMA ATUALIZACAO Dos ComponentesPrincipalPorSKU   EM MENOS DE 1 HORA - 60 MINUTOS')
