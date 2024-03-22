e8363b091d82755f8b59ba1f9871ea845a8ed028
luis-fe <luisengp@gmail.com>
2024-03-22 09:11:31 -0300

    commit incrementado dados opsc

--- RecarregarBanco.py
+++ RecarregarBanco.py
@@ -64,6 +64,7 @@ def FilaTags():
     df_tags = pd.merge(df_tags,restringe,on=['numeroop','cor'],how='left')
 
     df_tags['resticao'].fillna('-',inplace=True)
+    df_tags['considera'].fillna('-',inplace=True)
 
     #try:
     tamanho2 = 1000
