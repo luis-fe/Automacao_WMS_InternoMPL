b586d1521a940587b063c67a3f046f8a977db3bf
luis-fe <luisengp@gmail.com>
2024-04-08 17:26:27 -0300

    commit incrementado dados opsc

--- InformacoesOPCsw/pedios.py
+++ InformacoesOPCsw/pedios.py
@@ -10,24 +10,8 @@ def IncrementarPedidos():
 
     pedidos = pd.merge(pedidos,sugestoes,on=['codPedido','codProduto'],how='left')
 
-    conn.close()
-    pedidos['Marca'] ='-'
-    pedidos['Marca'] = pedidos.apply(lambda row :  Marca(row['coditemPai'],row['Marca']) ,axis=1)
+
 
     # Carregando dados no Wms
     ConexaoPostgreMPL.Funcao_InserirPCP(pedidos, pedidos['codPedido'].size, 'pedidosItemgrade', 'replace')
-
-
-def Marca(coditempai, marca):
-    coditempai = str(coditempai)
-    if coditempai == '202' and marca =='-':
-        return 'MPOLLO'
-    elif coditempai == '102'and marca =='-':
-        return 'MPOLLO'
-    elif coditempai == '104'and marca =='-':
-        return 'PACO'
-    elif coditempai == '204'and marca =='-':
-        return 'PACO'
-    else:
-        return '-'
 
