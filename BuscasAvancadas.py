3040078d9bb2a93f873eeccd346ae4db656dc38e
luis-fe <luisengp@gmail.com>
2024-04-09 08:24:18 -0300

    commit incrementado dados opsc

--- BuscasAvancadas.py
+++ BuscasAvancadas.py
@@ -329,7 +329,7 @@ order by codPedido desc"""
     return consulta
 
 def SugestaoItemAberto():
-    consulta = """SELECT top 1000000 p.codPedido, p.codProduto , p.qtdePedida ,  p.qtdeFaturada, p.qtdeCancelada,
+    consulta = """SELECT top 500000 p.codPedido, p.codProduto , p.qtdePedida ,  p.qtdeFaturada, p.qtdeCancelada,
 (SELECT i.nome from cgi.Item i WHERE i.codigo = p.codProduto) as nome,
 (SELECT i2.coditempai from cgi.Item2 i2 WHERE i2.coditem = p.codProduto and i2.empresa = 1) as coditemPai,
 (SELECT case when sUBSTRING(i2.coditempai,1,3) ='104' THEN 'PACO' 
