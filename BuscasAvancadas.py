b655376e24f4fb3789f7a40e6941ede76cbb5609
luis-fe <luisengp@gmail.com>
2024-04-11 10:40:12 -0300

    commit incrementado dados opsc

--- BuscasAvancadas.py
+++ BuscasAvancadas.py
@@ -323,7 +323,7 @@ SELECT codigo as codFornecNota , nome as nomeFornecedor  FROM Cpg.Fornecedor f W
 
 def IncrementarPediosProdutos():
     consulta = """SELECT top 1000000 p.codPedido, p.codProduto , p.qtdePedida ,  p.qtdeFaturada, p.qtdeCancelada  FROM ped.PedidoItemGrade p
-WHERE p.codEmpresa = 1 and p.codProduto  not like '8601000%' or p.codProduto  not like '830600620%'
+WHERE p.codEmpresa = 1 and p.codProduto  not like '8601000%' and p.codProduto  not like '830600620%'
 order by codPedido desc"""
 
     return consulta
