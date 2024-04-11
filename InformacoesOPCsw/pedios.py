05bd6da8dc27bb20e8fef66c048dd117149d91b8
luis-fe <luisengp@gmail.com>
2024-04-11 12:04:12 -0300

    commit incrementado dados opsc

--- InformacoesOPCsw/pedios.py
+++ InformacoesOPCsw/pedios.py
@@ -18,7 +18,7 @@ def IncrementarPedidos():
     conn.close()#Fechando a Conexao com o CSW
 
     pedidos['codTipoNota'] = pedidos['codTipoNota'].astype(str)
-    pedidos = pedidos[(pedidos['codTipoNota'] != '38') & (pedidos['codTipoNota'] != '239')]
+    pedidos = pedidos[(pedidos['codTipoNota'] != '38') & (pedidos['codTipoNota'] != '239') & (pedidos['codTipoNota'] != '223')]
 
     # Carregando dados no Wms
     ConexaoPostgreMPL.Funcao_InserirPCP(pedidos, pedidos['codPedido'].size, 'pedidosItemgrade', 'replace')
