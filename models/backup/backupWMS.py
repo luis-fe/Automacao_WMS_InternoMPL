7c0accb230caa18398e77e7e387f06b2b354b7c8
luis-fe <luisengp@gmail.com>
2024-04-16 10:49:11 -0300

    automacao 10 etapas

--- models/backup/backupWMS.py
+++ models/backup/backupWMS.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import ConexaoPostgreMPL
+import empresaConfigurada
 
 
 def Backuptagsreposicao():
+    emp = empresaConfigurada.EmpresaEscolhida()
     conn = ConexaoPostgreMPL.conexao()
 
     consulta = pd.read_sql('select * from "Reposicao".tagsreposicao',conn)
 
-    conn.close()
+    if emp == '1':
+        consulta2 = pd.read_sql('select * from "off".reposicao_qualidade',conn)
+        consulta2.to_csv('backup_reposicao_qualidade.csv')
+
     consulta.to_csv('backup_tagsreposicao.csv')
+    conn.close()
