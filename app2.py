302a9b86195605d55b1316be6a13279d6c7fa5c2
luis-fe <luisengp@gmail.com>
2024-08-29 09:19:34 -0300

    implantacao do atualizar pedidos app2 /2

--- app2.py
+++ app2.py
@@ -128,13 +128,10 @@ if __name__ == '__main__':
 
     automacao()
     os.system('clear')
-
-
-
     # Iniciar nova instância do script após N segundos
     new_process = f"{sys.executable} {sys.argv[0]}"
     print(f'gerado o process {new_process}')
-    os.system(f"sleep 180 && {new_process} &")
+    os.system(f"sleep 600 && {new_process} &")
 
     #Encerrando o Registro de controle do PID
     controle.excluirPID()
